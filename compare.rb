f2f6bc321eea2a72c8fc1c3d4bc2b4558aa2035a
nakazawa <zhongzejiangji@gmail.com>
2023-07-21 03:04:35 +0000

    lesson5 add

---
+++ compare.rb
@@ -0,0 +1,10 @@
+total = 100
+# totalが200より小さい場合、真の値となるtrueが返り処理が実行される
+if total < 200
+  puts "totalは200未満です"
+end
+
+# totalが150以下の場合、条件に当てはまらず偽の値が返されるため処理が実行されない
+if total >= 150
+  puts "totalは150以上です"
+end
